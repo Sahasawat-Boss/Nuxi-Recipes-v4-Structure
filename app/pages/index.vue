852bf969b8508141ad54cb3eddcac6d2a682b152
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-04-19 06:46:52 +0700

    u index and md

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,20 +1,20 @@
 <template>
     <main>
         <section class="bg-[#f1f1f1]">
-            <div class="container flex flex-col lg:flex-row items-center py-20 gap-10">
+            <div class="container flex flex-col lg:flex-row items-center py-12 gap-10">
                 <div class="flex-1 order-2 lg:order-1 text-center lg:text-left">
-                    <h1 class="text-4xl lg:text-6xl font-extrabold mb-6 text-balance">
+                    <h1 class="text-4xl lg:text-5xl bold mb-6 text-balance">
                         Master the Kitchen with Ease: Unleash Your Inner Chef Today!
                     </h1>
                     <p class="text-xl lg:text-2xl mb-8 text-balance">
                         Discover recipes helping you to find the easiest way to cook.
                     </p>
-                    <button class="px-4 py-2 text-white self-start bg-dodgeroll-gold rounded-md text-lg cursor-pointer">
+                    <button class="px-4 py-2 text-white self-start bg-amber-600/95 rounded-md text-lg cursor-pointer">
                         Browse Recipes
                     </button>
                 </div>
                 <div class="flex-1 order-1 lg:order-2">
-                    <NuxtImg sizes="70vw md:400px" src="/nuxt-course-hero.png"  densities="x1" alt="hero image" />
+                    <NuxtImg sizes="70vw md:400px" src="/nuxt-course-hero.png" format="webp"  densities="x1" alt="hero image" />
                 </div>
             </div>
         </section>
