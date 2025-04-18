7cdf8411495435c384cbdb948227671ada0f3267
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-04-18 15:37:45 +0700

    i module and u configs

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-4xl text-blue-500 font-bold">Hello</h1>
+    <p class="text-black bg-amber-50 p-4">text</p>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,55 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  experimental: {
+    scanPageMeta: 'after-resolve',
+    sharedPrerenderData: false,
+    compileTemplate: true,
+    resetAsyncDataToUndefined: true,
+    templateUtils: true,
+    relativeWatchPaths: true,
+    normalizeComponentNames: false,
+    spaLoadingTemplateLocation: 'within',
+    // parseErrorData: false,
+    defaults: {
+      useAsyncData: {
+        deep: true
+      }
+    }
+  },
+
+  features: {
+    inlineStyles: true
+  },
+
+  unhead: {
+    renderSSRHeadOptions: {
+      omitLineBreaks: false
+    }
+  },
+
+  devtools: { enabled: true },
+  css: ['/assets/css/global.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  modules: [
+    '@nuxtjs/google-fonts',
+    '@nuxt/icon',
+    '@nuxt/image'
+  ],
+
+  googleFonts: {
+    families: {
+      Montserrat: true,
+  }},
 })
